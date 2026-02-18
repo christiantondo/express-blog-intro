14797e9df17dae5bf6dd8fe99b54e2ab313be62e
christiantondo <christiantondo1@gmail.com>
2026-02-18 02:21:02 +0100

    characters array moved to indipendent file

---
+++ data/characters.js
@@ -0,0 +1,34 @@
+const characters = [
+    {
+        title: "Father Gascoigne",
+        content: "Father Gascoigne (ガスコイン神父 Gasukoin shinpu lit. Father Gascoigne) is a Boss in Bloodborne. He can first be encountered as a summon against the Cleric Beast, and then he will be encountered again as a boss in Tomb of Oedon.",
+        img: "/imgs/characters/father-gascoigne.jpeg",
+        tags: ["Bloodborne", "Bosses", "Main Story"]
+    },
+    {
+        title: "Vicar Amelia",
+        content: "Vicar Amelia (教区長エミーリア Kyōku-chō Emīria lit. Vicar Amelia/Emilia) is a Boss in Bloodborne. Vicar Amelia is required to progress through the game and defeating her will turn the world state into night time. She is a human that slowly turns into a variant of a Cleric Beast, making her vulnerable to Fire and Serration damage.",
+        img: "/imgs/characters/vicar-amelia.jpeg",
+        tags: ["Bloodborne", "Bosses", "Main Story"]
+    },
+    {
+        title: "Amygdala",
+        content: "Amygdala (アメンドーズ Amendōzu - Amendoze) is an optional Great One Boss in Bloodborne. The Amygdala is similar to the large enemies who attempt to grab the Hunter with portals, but lighter in color, and lacking tentacles on its head.",
+        img: "/imgs/characters/amygdala.jpeg",
+        tags: ["Bloodborne", "Bosses", "Main Story"]
+    },
+    {
+        title: "Lady Maria of the Astral Clocktower",
+        content: "Lady Maria of the Astral Clocktower (時計塔のマリア Tokei-tō no Maria lit. Maria of the Clocktower) is a Boss in Bloodborne. She was added in the release of The Old Hunters DLC.",
+        img: "/imgs/characters/lady-maria.jpeg",
+        tags: ["Bloodborne", "Bosses", "The Old Hunters"]
+    },
+    {
+        title: "Gehrman, The First Hunter",
+        content: "Gehrman, the First Hunter is a Boss in Bloodborne. Gehrman, the First Hunter uses the Burial Blade and a unique version of the Hunter Blunderbuss as his weapons of choice. Gehrman uses quick attacks and his agility to maneuver around the arena, using his combat prowess to gain an advantage in the fight. For the NPC version of Gehrman, you may visit this page for more information.",
+        img: "/imgs/characters/gehrman-the-first-hunter.jpeg",
+        tags: ["Bloodborne", "Bosses", "Main Story"]
+    },
+]
+
+module.exports = characters;
