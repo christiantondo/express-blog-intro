24e7d31b72e7b3c002d34886ff4e6c281d729366
christiantondo <christiantondo1@gmail.com>
2026-02-18 01:53:44 +0100

    img import

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-console.log("Benvenuto nel mondo server-side");
 
 const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.send('Server del mio blog')
+})
+
+app.get('/bacheca', (req, res) => {
+    res.json('Benvenuto nella sezione bacheca')
 })
 
 app.listen(port, () => {
